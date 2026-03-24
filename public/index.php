a4a414b94d954c709cba561094255690afd84684
clementbo <clementbo50@gmail.com>
2026-03-24 18:45:10 +0100

    Switch to Composer autoloader and remove manual spl_autoload_register

--- public/index.php
+++ public/index.php
@@ -4,22 +4,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
-spl_autoload_register(function ($class) {
-    $prefix = 'App\\';
-    $baseDir = __DIR__ . '/../src/';
-    
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-    
-    $relativeClass = substr($class, $len);
-    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
-    
-    if (file_exists($file)) {
-        require $file;
-    }
-});
 
 require_once __DIR__ . '/../config/database.php';
 
